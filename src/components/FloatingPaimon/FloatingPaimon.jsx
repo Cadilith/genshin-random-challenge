efd447efa5ea288468b22af3fa26732938bee53a
Tiffanie Orsoni <tiffanie.orsoni@gmail.com>
2023-09-21 14:58:26 +0200

    floating paimon component init

---
+++ src/components/FloatingPaimon/FloatingPaimon.jsx
@@ -0,0 +1,9 @@
+function FloatingPaimon() {
+    return (
+        <>
+            <img className="paimon floating" src='../assets/img/paimon.png' alt="Paimon is waiting for generation" />
+        </>
+    )
+}
+
+export default FloatingPaimon
